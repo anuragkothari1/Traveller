df32a856a5893d56449f4f4e5783e36b2aefdd48
anuragkothari1 <anukothari111@gmail.com>
2024-04-10 19:33:47 +0530

    m

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
+
+const menu = document.getElementById("ul");
+menu.style.left="100%"
 const menuClicked = () => {
     console.log("clicked");
     const menu = document.getElementById("ul");
-    const body = document.body;
-    const m=document.getElementById("menu");
+  
 
     if (menu.style.left === "0px" || menu.style.left === "") {
         menu.style.left = "100%";
-        // body.style.overflow = "auto";
-        // body.style.position = "static"; 
-      
-     // Prevent the body from moving
+     
     } else {
         menu.style.left = "0";
-        // body.style.overflow = "hidden";
-        // body.style.position = "fixed";
-        
+     
     }
 };
 document.addEventListener("scroll",()=>{
