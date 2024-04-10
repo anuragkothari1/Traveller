28082aa8d7d0bfeaf5ef5661287b8149e2648002
anuragkothari1 <anukothari111@gmail.com>
2024-04-10 14:35:48 +0530

    final

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const menuClicked = () => {
     console.log("clicked");
     const menu = document.getElementById("ul");
+    const body = document.body;
+    const m=document.getElementById("menu");
+
     if (menu.style.left === "0px" || menu.style.left === "") {
         menu.style.left = "100%";
-    
-        console.log("here1")
+        // body.style.overflow = "auto";
+        // body.style.position = "static"; 
+      
+     // Prevent the body from moving
     } else {
         menu.style.left = "0";
-        console.log("here")
+        // body.style.overflow = "hidden";
+        // body.style.position = "fixed";
+        
     }
 };
+document.addEventListener("scroll",()=>{
+    
+    const navbar= document.getElementById("nav");
+   
+    if (window.scrollY>50){
+        navbar.style.backgroundColor="white";
+        console.log("here")
+       
+       
+    }else{
+        navbar.style.backgroundColor="";
+        console.log("here2")
+    
+        
+    }
+})
